8331a2238cb8f213ddb81fb82eb849b62d3aeea0
soysauce1223 <soysauce1223@naver.com>
2025-07-07 17:40:50 +0900

    [feat] 회원가입 1번째 화면 구현: 멘토인지 멘티인지 선택

---
+++ worlds-fe-v20/Views/SignUp/SignUpRoleSelectionView.swift
@@ -0,0 +1,106 @@
+//
+//  SignUpRoleSelectionView.swift
+//  worlds-fe-v20
+//
+//  Created by seohuibaek on 7/7/25.
+//
+
+import SwiftUI
+
+/// 회원가입 1번째 페이지 - 멘토/멘티 선택 화면
+struct SignUpRoleSelectionView: View {
+    @State var selectedRole: UserRole = .none
+    @State var isSelected: Bool = false
+    @State var isSuceed: Bool = false
+        
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            Text("누구의 계정인가요?")
+                .font(.system(size: 27))
+            
+            HStack(alignment: .center) {
+                
+                Spacer()
+                
+                Button {
+                    print("mentor")
+                    selectedRole = .mentor
+                    isSelected = true
+                } label: {
+                    VStack {
+                        Image("mentor")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding()
+                        
+                        Spacer()
+                        
+                        Text("멘토")
+                            .font(.system(size: 20))
+                            .foregroundStyle(Color.black)
+                    }
+                    .padding()
+                    .background(selectedRole == .mentor ? Color.beigews.opacity(0.2) : Color.white)
+                    .cornerRadius(16)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(Color.beigews, lineWidth: 2)
+                            .opacity(selectedRole == .mentor ? 1 : 0)
+                    )
+                    .frame(width: 155, height: 166)
+                }
+                
+                Button {
+                    print("mentee")
+                    selectedRole = .mentee
+                    isSelected = true
+                } label: {
+                    VStack {
+                        Image("mentee")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        
+                        Spacer()
+                        
+                        Text("멘티")
+                            .font(.system(size: 20))
+                            .foregroundStyle(Color.black)
+                    }
+                    .padding()
+                    .background(selectedRole == .mentee ? Color.beigews.opacity(0.2) : Color.white)
+                    .cornerRadius(16)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(Color.beigews, lineWidth: 2)
+                            .opacity(selectedRole == .mentee ? 1 : 0)
+                    )
+                    .frame(width: 155, height: 166)
+                }
+                
+                Spacer()
+            }
+            
+            Spacer()
+
+            CommonSignUpButton(text: "다음", isFilled: $isSelected) {
+                // viewmodel에 데이터 전송
+                print("SignUpAccountView")
+                
+                // viewModel 호출 후 화면 전환 (어떤 방식이 더 효율적인지는 아직 모르겠음)
+                isSuceed = true
+            }
+        }
+        .padding()
+        .background(.backgroundws)
+        .navigationTitle("회원가입")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationDestination(isPresented: $isSuceed) {
+            SignUpAccountView()
+        }
+    }
+}
+
+#Preview {
+    SignUpRoleSelectionView()
+}
