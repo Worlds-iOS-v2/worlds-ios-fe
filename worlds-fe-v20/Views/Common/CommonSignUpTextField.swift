c9c37d52d8d0f6d17bee9033cf599611c15c0205
soysauce1223 <soysauce1223@naver.com>
2025-07-07 17:42:17 +0900

    [feat] 회원가입 시 공통으로 사용하는 TextField 추가

---
+++ worlds-fe-v20/Views/Common/CommonSignUpTextField.swift
@@ -0,0 +1,36 @@
+//
+//  CommonSignUpTextField.swift
+//  worlds-fe-v20
+//
+//  Created by seohuibaek on 7/7/25.
+//
+
+import SwiftUI
+
+struct CommonSignUpTextField: View {
+    @State var title: String = ""
+    @State var placeholder: String = ""
+    @Binding var content: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("\(title)")
+                .foregroundStyle(Color.gray)
+                .font(.system(size: 20))
+                .fontWeight(.semibold)
+            
+            TextField("\(placeholder)", text: $content)
+                .foregroundStyle(Color.gray)
+                .font(.system(size: 20))
+                .fontWeight(.semibold)
+                .frame(height: 50)
+                .padding(.horizontal, 14)
+                .background(Color.white)
+                .cornerRadius(10)
+        }
+    }
+}
+
+#Preview {
+    // CommonSignUpTextField(title: "이메일", placeholder: "이메일을 입력해주세요.")
+}
