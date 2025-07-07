37afe95086d5211f54ab71782bdd0ac199fd6304
soysauce1223 <soysauce1223@naver.com>
2025-07-07 17:42:34 +0900

    [feat] 회원가입 하단에서 공통으로 사용하는 버튼 추가

---
+++ worlds-fe-v20/Views/Common/CommonSignUpButton.swift
@@ -0,0 +1,35 @@
+//
+//  CommonSignUpButton.swift
+//  worlds-fe-v20
+//
+//  Created by seohuibaek on 7/7/25.
+//
+
+import SwiftUI
+
+struct CommonSignUpButton: View {
+    var text: String
+    @Binding var isFilled: Bool
+    var action: () -> Void
+    
+    var body: some View {
+        Button (action: action) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(isFilled ? Color.brownws: Color.gray)
+                    .frame(height: 60)
+                
+                Text("\(text)")
+                    .font(.system(size: 24))
+                    .foregroundStyle(.white)
+            }
+        }
+        .disabled(!isFilled)
+    }
+}
+
+#Preview {
+//    CommonSignUpButton(text: "다음") {
+//        
+//    }
+}
