5f0c4f7255aad14f5439a226d1f24f0b4f51ef74
soysauce1223 <soysauce1223@naver.com>
2025-07-15 15:01:46 +0900

    [Style] 상단 안내 문구 Text 추가 및 툴바 커스텀

--- worlds-fe-v20/Views/SignUp/SignUpAccountView.swift
+++ worlds-fe-v20/Views/SignUp/SignUpAccountView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 /// 회원가입 2번째 화면 - 이메일, 비밀번호 입력
 struct SignUpAccountView: View {
     @EnvironmentObject var appState: AppState
+    @Environment(\.dismiss) var dismiss
 
     @State var email: String = ""
     @State var password: String = ""
@@ -29,36 +30,42 @@ struct SignUpAccountView: View {
     
     var body: some View {
         VStack {
-            CommonSignUpTextField(title: "이메일", placeholder: "이메일을 입력해주세요", content: $email)
-                .keyboardType(.emailAddress)
-                .padding(.bottom, 40)
-                .padding(.top, 80)
-            
-//            if !email.isEmpty && !isValidEmail(email) {
-//                Text("올바른 이메일 형식이 아닙니다.")
-//                    .foregroundColor(.red)
-//                    .font(.caption)
-//            }
-            
-            CommonSignUpTextField(title: "비밀번호", placeholder: "비밀번호를 입력해주세요", isSecure: true, content: $password)
-                .padding(.bottom, 40)
-                        
-            CommonSignUpTextField(title: "비밀번호 확인", placeholder: "비밀번호를 한 번 더 입력해주세요.", isSecure: true, content: $passwordCheck)
-                .padding(.bottom, 40)
-            
-            Spacer()
-            
-            CommonSignUpButton(text: "다음", isFilled: isFilled) {
-                // viewmodel에 데이터 전송
-                print("SignUpDetailProfileView")
+            VStack(alignment: .leading) {
+                Text("로그인 정보를 입력해주세요.")
+                    .font(.system(size: 27, weight: .bold))
+                    .padding(.top, 40)
                 
-                viewModel.email = email
-                viewModel.password = password
+                CommonSignUpTextField(title: "이메일", placeholder: "이메일을 입력해주세요", content: $email)
+                    .keyboardType(.emailAddress)
+                    .padding(.bottom, 40)
+                    .padding(.top, 40)
                 
-                // viewModel 호출 후 화면 전환 (어떤 방식이 더 효율적인지는 아직 모르겠음)
-                isSuceed = true
+                //            if !email.isEmpty && !isValidEmail(email) {
+                //                Text("올바른 이메일 형식이 아닙니다.")
+                //                    .foregroundColor(.red)
+                //                    .font(.caption)
+                //            }
+                
+                CommonSignUpTextField(title: "비밀번호", placeholder: "비밀번호를 입력해주세요", isSecure: true, content: $password)
+                    .padding(.bottom, 40)
+                
+                CommonSignUpTextField(title: "비밀번호 확인", placeholder: "비밀번호를 한 번 더 입력해주세요.", isSecure: true, content: $passwordCheck)
+                    .padding(.bottom, 40)
+                
+                Spacer()
+                
+                CommonSignUpButton(text: "다음", isFilled: isFilled) {
+                    // viewmodel에 데이터 전송
+                    print("SignUpDetailProfileView")
+                    
+                    viewModel.email = email
+                    viewModel.password = password
+                    
+                    // viewModel 호출 후 화면 전환 (어떤 방식이 더 효율적인지는 아직 모르겠음)
+                    isSuceed = true
+                }
+                .padding(.bottom, 12)
             }
-            .padding(.bottom, 12)
             
             Button {
                 appState.flow = .login
@@ -72,6 +79,18 @@ struct SignUpAccountView: View {
         .background(.backgroundws)
         .navigationTitle("회원가입")
         .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .foregroundColor(.black)
+                        .font(.system(size: 18, weight: .semibold))
+                }
+            }
+        }
         .navigationDestination(isPresented: $isSuceed) {
             SignUpDetailProfileView()
         }
