387418858b7edd31ebbb59db88c731d3460cb81f
soysauce1223 <soysauce1223@naver.com>
2025-07-21 12:02:47 +0900

    [refactor] 기존 모델 형식 변경에 따른 반영

--- worlds-fe-v20/Models/User.swift
+++ worlds-fe-v20/Models/User.swift
@@ -9,28 +9,10 @@ import Foundation
 
 struct User: Codable, Hashable, Identifiable {
     let id: Int
-    let name: String
-    let birthDate: Date
-    let email: String
-    let password: String
-    let role: UserRole
-    let roleGrade: RoleGrade?
+    let userName: String
+    let birthday: Date
+    let userEmail: String
+    let isMentor: Bool
     let reportedCount: Int
-    let point: Int
-    let koreanGrade: KoreanGrade?
-    let refreshToken: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case name = "user_name"
-        case birthDate = "user_birth_date"
-        case email = "user_email"
-        case password = "user_password_hash"
-        case role = "user_role"
-        case roleGrade = "user_role_grade"
-        case reportedCount = "user_reported_count"
-        case point = "user_point"
-        case koreanGrade = "user_korean_grade"
-        case refreshToken = "user_refresh_token"
-    }
+    let menteeTranslations: [String?]
 }
