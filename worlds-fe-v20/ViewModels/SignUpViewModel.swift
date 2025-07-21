3cd65b113f63b89b30d617e20cbcfd95ae3856ad
soysauce1223 <soysauce1223@naver.com>
2025-07-21 12:05:54 +0900

    [feat] 회원가입 함수 호출 및 이메일 인증 함수 호출

--- worlds-fe-v20/ViewModels/SignUpViewModel.swift
+++ worlds-fe-v20/ViewModels/SignUpViewModel.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 final class SignUpViewModel: ObservableObject {
-    @Published var role: UserRole = .none
+//    @Published var role: UserRole = .none
+    @Published var isMentor: Bool = false
     @Published var email: String = ""
     @Published var password: String = ""
     @Published var name: String = ""
-    @Published var phoneNumber: String = ""
-    @Published var birthDate = Date()
+//    @Published var phoneNumber: String = ""
+    @Published var birthDate: String = ""
+    @Published var mentorCode: String?
     
-    // 추후 회원가입 함수
+    @Published var errorMessage: String?
+    
+    // 회원가입 함수
+    @MainActor
+    func signup() async {
+        do {
+            print("email: \(email), password: \(password)")
+            let user = try await UserAPIManager.shared.signUp(name: name, email: email, password: password, birth: birthDate, isMentor: isMentor, mentorCode: mentorCode)
+        } catch {
+            self.errorMessage = error.localizedDescription
+        }
+    }
+    
+    // 이메일 인증
+    func checkEmail() async {
+        do {
+            let user = try await UserAPIManager.shared.emailCheck(email: email)
+        } catch {
+            self.errorMessage = error.localizedDescription
+        }
+    }
 }
