9ce7e38e663a9c528cddf79be65a166aba7720b9
soysauce1223 <soysauce1223@naver.com>
2025-07-31 17:09:50 +0900

    [feat] viewmodel에서 유저 이름 불러오기 및 게시물 리스트 받아서 최신 게시물 확인할 수 있도록 구현

--- worlds-fe-v20/ViewModels/MainViewModel.swift
+++ worlds-fe-v20/ViewModels/MainViewModel.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 final class MainViewModel: ObservableObject {
-    @Published var email: String = ""
-    @Published var password: String = ""
-    @Published var name: String = ""
-    @Published var phoneNumber: String = ""
-    @Published var birthDate = Date()
+    @Published var errorMessage: String?
+    @Published var posts: [QuestionList] = []
+
+    func getUsername() -> String {
+        guard let username = UserDefaults.standard.string(forKey: "username") else {
+            return "사용자 이름을 불러올 수 없습니다."
+        }
+        return username
+    }
+    
+    @MainActor
+    func fetchLatestPosts() async {
+        do {
+            let posts = try await APIService.shared.fetchQuestions()
+            self.posts = posts
+            self.errorMessage = nil
+        } catch {
+            print("❌ 에러 발생:", error)
+            self.errorMessage = "질문 최신 목록을 불러오는데 실패했습니다: \(error.localizedDescription)"
+        }
+    }
 }
