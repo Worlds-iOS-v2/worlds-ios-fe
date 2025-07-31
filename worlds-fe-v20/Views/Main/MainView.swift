33015f9e0bd5d58bc696224c05095b30b359e867
soysauce1223 <soysauce1223@naver.com>
2025-07-31 18:34:08 +0900

    [fix] conflict 해결

--- worlds-fe-v20/Views/Main/MainView.swift
+++ worlds-fe-v20/Views/Main/MainView.swift
@@ -17,97 +17,106 @@ struct MainView: View {
         NavigationStack {
             ScrollView {
                 VStack {
-                    Text("안녕하세요! \(viewModel.getUsername())님")
-                        .font(.system(size: 27))
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                    VStack {
+                        Text("안녕하세요! \(viewModel.getUsername())님")
+                            .font(.system(size: 27))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.horizontal, 24)
+                        
+                        // 출석 체크 화면
+                        ZStack() {
+                            RoundedRectangle(cornerRadius: 16)
+                                .fill(Color.white)
+                                .frame(height: 120)
+                                .frame(maxWidth: .infinity)
+                                .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 4)
+                            
+                            HStack() {
+                                ForEach(1...7, id: \.self) { weekday in
+                                    AttendanceRowView(
+                                        weekday: weekday,
+                                        isAttended: attendanceData[weekday] ?? true,
+                                        isToday: weekday == Calendar.current.component(.weekday, from: Date()))
+                                }
+                            }
+                        }
                         .padding(.horizontal, 24)
-                }
-                .background(
-                    Rectangle()
-                        .fill(Color.sub2Ws)
-                        .padding(.bottom, 60)
-                        .padding(.top, -120)
-                )
-                .padding(.vertical, 40)
-                
-                HStack() {
-                    Text("이번주 소식")
+                    }
+                    .background(
+                        Rectangle()
+                            .fill(Color.sub2Ws)
+                            .padding(.bottom, 60)
+                            .padding(.top, -120)
+                    )
+                    .padding(.vertical, 40)
+                    
+                    HStack() {
+                        Text("이번주 소식")
+                            .font(.system(size: 27))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.horizontal, 24)
+                            .padding(.bottom, 20)
+                        
+                        NavigationLink(destination: CultureDetailView()) {
+                            Text("더보기 >")
+                                .font(.system(size: 16))
+                                .foregroundStyle(Color.mainws)
+                                .padding(.horizontal, 24)
+                                .padding(.bottom, 20)
+                        }
+                    }
+                    
+                    AutoSlideViewWithTimer()
+                        .frame(height: 300)
+                        .padding(.horizontal, 24)
+                    
+                    Text("최신글")
                         .font(.system(size: 27))
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.horizontal, 24)
                         .padding(.bottom, 20)
                     
-                    NavigationLink(destination: CultureDetailView()) {
-                        Text("더보기 >")
-                            .font(.system(size: 16))
-                            .foregroundStyle(Color.mainws)
-                            .padding(.horizontal, 24)
-                            .padding(.bottom, 20)
-                    }
-                }
-                .padding(.horizontal, 24)
-            }
-            .background(
-                Rectangle()
-                    .fill(Color.sub2Ws)
-                    .padding(.bottom, 60)
-                    .padding(.top, -120)
-            )
-            .padding(.vertical, 40)
-            
-            Text("이번주 소식")
-                .font(.system(size: 27))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal, 24)
-                .padding(.bottom, 20)
-            
-            AutoSlideViewWithTimer()
-                .frame(height: 300)
-                .padding(.horizontal, 24)
-            
-            Text("최신글")
-                .font(.system(size: 27))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal, 24)
-                .padding(.bottom, 20)
-            
-            VStack(spacing: 16) {
-                Spacer()
-                
-                ForEach(viewModel.posts.prefix(5), id: \.self) { post in
-                    Button {
-                        // 해당 게시물로 화면 이동
-                    } label: {
-                        HStack(spacing: 40){
-                            Text("\(post.category.displayName)")
-                                .font(.system(size: 18))
-                                .foregroundStyle(Color.black)
-                                .frame(width: 40, alignment: .leading)
-                            
-                            Text("\(post.title)")
-                                .font(.system(size: 18))
-                                .foregroundStyle(Color.black)
-                                .lineLimit(1)
-                                .truncationMode(.tail)
-                                .frame(maxWidth: .infinity, alignment: .leading)
+                    VStack(spacing: 16) {
+                        Spacer()
+                        
+                        ForEach(viewModel.posts.prefix(5), id: \.self) { post in
+                            NavigationLink(destination: QuestionDetailView(questionId: post.id, viewModel: QuestionViewModel())) {
+                                
+                                HStack(spacing: 40){
+                                    Text("\(post.category.displayName)")
+                                        .font(.system(size: 18))
+                                        .foregroundStyle(Color.black)
+                                        .frame(width: 40, alignment: .leading)
+                                    
+                                    Text("\(post.title)")
+                                        .font(.system(size: 18))
+                                        .foregroundStyle(Color.black)
+                                        .lineLimit(1)
+                                        .truncationMode(.tail)
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                }
+                                .padding(.horizontal, 48)
+                                
+                            }
                         }
+                        
+                        Spacer()
                     }
-                    Spacer()
+                    .background(
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(.backgroundws)
+                            .padding(.horizontal, 24)
+                            .shadow(color: .black.opacity(0.25), radius: 4, x: 4, y: 4)
+                    )
                 }
-                .background(
-                    RoundedRectangle(cornerRadius: 16)
-                        .fill(.backgroundws)
-                        .padding(.horizontal, 24)
-                        .shadow(color: .black.opacity(0.25), radius: 4, x: 4, y: 4)
-                )
+                .padding(.bottom, 100)
+                .background(Color.white)
             }
-            .padding(.bottom, 100)
-            .background(Color.white)
-        }
-        .scrollIndicators(.hidden)
-        .onAppear {
-            Task {
-                try await viewModel.fetchLatestPosts()
+            .scrollIndicators(.hidden)
+            .onAppear {
+                Task {
+                    await viewModel.fetchLatestPosts()
+                }
             }
         }
     }
