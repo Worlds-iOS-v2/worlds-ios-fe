5ddf4203df2b9ffd92a0d3f55521d41d33768b32
soysauce1223 <soysauce1223@naver.com>
2025-08-04 14:32:13 +0900

    [refactor] NavigationStack을 TabView가 있는 TabbarVieew에만 선언해주어 하위 뷰로 넘어갔을 때 하단 탭바가 보이지 않도록 수정 및 MyPage, Main, OCR뷰에서도 NavigationStack이 설정되어 있는 부분을 삭제

--- worlds-fe-v20/Views/Tabbar/TabbarView.swift
+++ worlds-fe-v20/Views/Tabbar/TabbarView.swift
@@ -9,38 +9,40 @@ import SwiftUI
 
 struct TabbarView: View {
     var body: some View {
-        TabView {
-            MainView(viewModel: MainViewModel())
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("Home")
-                }
-            
-            QuestionView(viewModel: QuestionViewModel())
-                .tabItem {
-                    Image(systemName: "questionmark.circle")
-                    Text("Question")
-                }
-            
-            OCRCameraView()
-                .tabItem {
-                    Image(systemName: "camera")
-                    Text("OCR")
-                }
-            
-            // 채팅
-            //ChatView()
-//                .tabItem {
-//                    Image(systemName: "message")
-//                    Text("Chat")
-//                }
-            
-            // 마이페이지
-            //MyPageView()
-//                .tabItem {
-//                    Image(systemName: "person")
-//                    Text("MyPage")
-//                }
+        NavigationStack {
+            TabView {
+                MainView(viewModel: MainViewModel())
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                
+                QuestionView(viewModel: QuestionViewModel())
+                    .tabItem {
+                        Image(systemName: "questionmark.circle")
+                        Text("Question")
+                    }
+                
+                OCRCameraView()
+                    .tabItem {
+                        Image(systemName: "camera")
+                        Text("OCR")
+                    }
+                
+                // 채팅
+                //ChatView()
+//                    .tabItem {
+//                        Image(systemName: "message")
+//                        Text("Chat")
+//                    }
+                
+                // 마이페이지
+                MyPageView()
+                    .tabItem {
+                        Image(systemName: "person")
+                        Text("MyPage")
+                    }
+            }
         }
     }
 }
