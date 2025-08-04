96218fd4946a95310fc2cfdd25f9a2413cd00bc1
soysauce1223 <soysauce1223@naver.com>
2025-08-04 14:32:13 +0900

    [refactor] NavigationStack을 TabView가 있는 TabbarVieew에만 선언해주어 하위 뷰로 넘어갔을 때 하단 탭바가 보이지 않도록 수정 및 MyPage, Main, OCR뷰에서도 NavigationStack이 설정되어 있는 부분을 삭제

--- worlds-fe-v20/Views/MyPage/MyPageView.swift
+++ worlds-fe-v20/Views/MyPage/MyPageView.swift
@@ -11,93 +11,91 @@ struct MyPageView: View {
     @Environment(\.openURL) var openURL
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                Color.sub2Ws
-                    .ignoresSafeArea()
-                
+        ZStack {
+            Color.sub2Ws
+                .ignoresSafeArea()
+            
+            VStack {
                 VStack {
-                    VStack {
-                        Text("마이페이지")
-                            .font(.system(size: 27))
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.horizontal, 24)
-                        
-                        UserInfoCardView()
-                            .cornerRadius(20)
-                            .shadow(color: .black.opacity(0.25), radius: 4, x: 4, y: 4)
-                            .padding(.horizontal, 16)
-                    }
-                    .padding(.vertical, 40)
+                    Text("마이페이지")
+                        .font(.system(size: 27))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal, 24)
                     
-                    VStack(spacing: 16) {
-                        NavigationLink(destination: MyQuestionView()) {
-                            Text("내가 쓴 글")
-                                .font(.system(size: 18))
-                                .foregroundStyle(Color.black)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                        .padding(.horizontal, 32)
-                        
-                        Divider()
-                            .padding(.horizontal, 32)
-                        
-                        NavigationLink(destination: UserInfoCardView()) {
-                            Text("로그아웃")
-                                .font(.system(size: 18))
-                                .foregroundStyle(Color.black)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                        .padding(.horizontal, 32)
-                        
-                        Divider()
-                            .padding(.horizontal, 32)
-                        
-                        Link("이용약관", destination: URL(string: "https://www.notion.so/World-Study-_2-0-0-1fc800c9877b80d6a86ce296013ec7d7?source=copy_link")!)
+                    UserInfoCardView()
+                        .cornerRadius(20)
+                        .shadow(color: .black.opacity(0.25), radius: 4, x: 4, y: 4)
+                        .padding(.horizontal, 16)
+                }
+                .padding(.vertical, 40)
+                
+                VStack(spacing: 16) {
+                    NavigationLink(destination: MyQuestionView()) {
+                        Text("내가 쓴 글")
                             .font(.system(size: 18))
                             .foregroundStyle(Color.black)
                             .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.horizontal, 32)
-                        
-                        Divider()
-                            .padding(.horizontal, 32)
-                        
-                        HStack {
-                            Text("버전 정보")
-                                .font(.system(size: 18))
-                                .foregroundStyle(Color.black)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            
-                            Spacer()
-                            
-                            Text("\(appVersion())")
-                                .font(.system(size: 18))
-                                .foregroundStyle(Color.gray)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                        }
-                        .padding(.horizontal, 32)
-                        
-                        Divider()
-                            .padding(.horizontal, 32)
-                        
-                        NavigationLink(destination: DeleteAccountView()) {
-                            Text("회원탈퇴")
-                                .font(.system(size: 18))
-                                .foregroundStyle(Color.red)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                        .padding(.horizontal, 32)
                     }
-                    .padding(.vertical, 24)
-                    .background(
-                        RoundedRectangle(cornerRadius: 16)
-                            .fill(.backgroundws)
-                            .padding(.horizontal, 16)
-                            .shadow(color: .black.opacity(0.25), radius: 4, x: 4, y: 4)
-                    )
+                    .padding(.horizontal, 32)
+                    
+                    Divider()
+                        .padding(.horizontal, 32)
+                    
+                    NavigationLink(destination: UserInfoCardView()) {
+                        Text("로그아웃")
+                            .font(.system(size: 18))
+                            .foregroundStyle(Color.black)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .padding(.horizontal, 32)
+                    
+                    Divider()
+                        .padding(.horizontal, 32)
+                    
+                    Link("이용약관", destination: URL(string: "https://www.notion.so/World-Study-_2-0-0-1fc800c9877b80d6a86ce296013ec7d7?source=copy_link")!)
+                        .font(.system(size: 18))
+                        .foregroundStyle(Color.black)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal, 32)
+                    
+                    Divider()
+                        .padding(.horizontal, 32)
+                    
+                    HStack {
+                        Text("버전 정보")
+                            .font(.system(size: 18))
+                            .foregroundStyle(Color.black)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        Spacer()
+                        
+                        Text("\(appVersion())")
+                            .font(.system(size: 18))
+                            .foregroundStyle(Color.gray)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    .padding(.horizontal, 32)
+                    
+                    Divider()
+                        .padding(.horizontal, 32)
+                    
+                    NavigationLink(destination: DeleteAccountView()) {
+                        Text("회원탈퇴")
+                            .font(.system(size: 18))
+                            .foregroundStyle(Color.red)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .padding(.horizontal, 32)
                 }
-                .padding(.bottom, 100)
+                .padding(.vertical, 24)
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(.backgroundws)
+                        .padding(.horizontal, 16)
+                        .shadow(color: .black.opacity(0.25), radius: 4, x: 4, y: 4)
+                )
             }
+            .padding(.bottom, 100)
         }
     }
 }
