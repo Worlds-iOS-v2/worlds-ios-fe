a7d92ff0dee190dc89be6ed2b3584abcaef291b2
soysauce1223 <soysauce1223@naver.com>
2025-08-08 21:15:10 +0900

    [style] 문화상세화면 UI 변경에 따른 반영

--- worlds-fe-v20/Views/CultureDetail/CultureDetailView.swift
+++ worlds-fe-v20/Views/CultureDetail/CultureDetailView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct CultureDetailView: View {
     @Environment(\.dismiss) var dismiss
-
     @StateObject var viewModel = CultureDetailViewModel()
     
     var body: some View {
@@ -24,83 +23,29 @@ struct CultureDetailView: View {
                         .padding(.horizontal, 24)
                         .padding(.vertical, 20)
                     
-                    AutoSlideViewWithTimer()
+                    AutoSlideViewWithTimer(datas: viewModel.dummyEventData)
                         .frame(height: 300)
                         .padding(.horizontal, 24)
+                        .padding(.bottom, 20)
                     
                     Text("정부 프로그램")
                         .font(.system(size: 27))
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.horizontal, 24)
-                        .padding(.bottom, 20)
                     
-                    VStack(spacing: 12) {
-                        Spacer()
-                        
-                        ForEach(0..<5, id: \.self) { _ in
-                            Button {
-                                // 해당 게시물로 화면 이동
-                            } label: {
-                                HStack(spacing: 40){
-                                    Text("제목")
-                                        .font(.system(size: 20))
-                                        .foregroundStyle(Color.black)
-                                    
-                                    Text("내용")
-                                        .font(.system(size: 20))
-                                        .foregroundStyle(Color.black)
-                                    
-                                    Spacer()
-                                }
-                                .padding(.horizontal, 48)
-                            }
-                        }
-                        Spacer()
-                    }
-                    .background(
-                        RoundedRectangle(cornerRadius: 16)
-                            .fill(.backgroundws)
-                            .padding(.horizontal, 24)
-                            .shadow(color: .black.opacity(0.25), radius: 4, x: 4, y: 4)
-                    )
-                }
-                .padding(.bottom, 20)
-                
-                Text("한국어 교육 프로그램")
-                    .font(.system(size: 27))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal, 24)
-                    .padding(.bottom, 20)
-                
-                VStack(spacing: 12) {
-                    Spacer()
-                    
-                    ForEach(0..<5, id: \.self) { _ in
-                        Button {
-                            // 해당 게시물로 화면 이동
-                        } label: {
-                            HStack(spacing: 40){
-                                Text("제목")
-                                    .font(.system(size: 20))
-                                    .foregroundStyle(Color.black)
-                                
-                                Text("내용")
-                                    .font(.system(size: 20))
-                                    .foregroundStyle(Color.black)
-                                
-                                Spacer()
-                            }
-                            .padding(.horizontal, 48)
-                        }
-                    }
-                    Spacer()
-                }
-                .background(
-                    RoundedRectangle(cornerRadius: 16)
-                        .fill(.backgroundws)
+                    CultureSlideView<GovernmentProgram>(datas: viewModel.dummyGovernmentData)
+                        .frame(height: 150)
                         .padding(.horizontal, 24)
-                        .shadow(color: .black.opacity(0.25), radius: 4, x: 4, y: 4)
-                )
+                    
+                    Text("한국어 교육 프로그램")
+                        .font(.system(size: 27))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal, 24)
+                    
+                    CultureSlideView<KoreanProgram>(datas: viewModel.dummyKoreanData)
+                        .frame(height: 150)
+                        .padding(.horizontal, 24)
+                }
                 .padding(.bottom, 20)
             }
             .navigationTitle("문화행사정보")
