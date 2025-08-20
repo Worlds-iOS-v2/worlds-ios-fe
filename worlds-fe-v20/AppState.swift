17d8680859b4a6f7a65789e4bd5d26f6e461484b
soysauce1223 <soysauce1223@naver.com>
2025-08-21 00:00:52 +0900

    [feat] 로그인 후 프로필 이미지가 없을 경우 이미지 선택화면으로 이동

--- worlds-fe-v20/AppState.swift
+++ worlds-fe-v20/AppState.swift
@@ -11,6 +11,7 @@ enum AppFlowState {
     case launch
     case login
     case signUp
+    case profileSelection // 프로필 이미지 선택 추가
     case main
 }
 
@@ -31,14 +32,36 @@ final class AppState: ObservableObject {
         do {
             // 2) 액세스 토큰 재발급 → 사용자 정보 조회
             _ = try await UserAPIManager.shared.getNewAccessToken()
-            _ = try await UserAPIManager.shared.getUserInfo()
+            let user = try await UserAPIManager.shared.getUserInfo()
 
-            // 3) 성공 시 메인으로
-            self.flow = .main
+            // 3) 프로필 이미지가 없으면 선택 화면으로, 있으면 메인으로
+            if user.userInfo?.profileImage == nil {
+                self.flow = .profileSelection
+            } else {
+                self.flow = .main
+            }
         } catch {
             // 4) 실패 시 보관 토큰 정리 후 로그인으로
             UserDefaults.standard.removeObject(forKey: "accessToken")
             UserDefaults.standard.removeObject(forKey: "refreshToken")
+            self.flow = .login
+        }
+    }
+    
+    /// 로그인 후 프로필 체크
+    @MainActor
+    func checkProfileAfterLogin() async {
+        do {
+            let user = try await UserAPIManager.shared.getUserInfo()
+            
+            // 프로필 이미지가 없으면 선택 화면으로, 있으면 메인으로
+            if user.userInfo?.profileImage == nil {
+                self.flow = .profileSelection
+            } else {
+                self.flow = .main
+            }
+        } catch {
+            // 사용자 정보 조회 실패 시 로그인으로
             self.flow = .login
         }
     }
